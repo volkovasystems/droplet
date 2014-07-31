ddb58c15f3f81347287c03ea8bf1ddeb24004367
Richeve S. Bebedor <volkovasystems@gmail.com>
2014-08-01 00:41:02 +0800

    Added createPrimaryCollection procedure.

--- class-drop.js
+++ class-drop.js
@@ -11,31 +11,109 @@ Drop.prototype.initialize = function initialize( accessToken ){
 }
 
 Drop.prototype.configure = function configure( ){
-	this.getCollectionList( function callback( ){
-		
+	var self = this;
+	this.getCollectionList( function callback( error, collectionList ){
+		if( error ){
+			throw error;
+		}else if( collectionList.length > 0 ){
+			self.setPrimaryCollection( collectionList[ 0 ] );
+		}else{
+			self.createPrimaryCollection( function callback( error, result ){
+				console.debug( arguments );
+			} );
+		}
 	} );
+};
+
+Drop.prototype.createPrimaryCollection = function createPrimaryCollection( callback ){
+	var accessToken = this.accessToken;
+	var primaryCollection = this.primaryCollection;
+
+	var DSIDParameter = [ "dsid", primaryCollection ].join( "=" );
+
+	boom( "POST", "https://api.dropbox.com/1/datastores/create_datastore",
+		function catcher( error ){
+			if( error ){
+				callback( error );
+				return;
+			}
+
+			if( this.readyState == 4 ){
+				if( this.status == 200 ){
+					var result = JSON.parse( this.responseText );
+					callback( null, result );
+
+				}else if( this.status >= 400 ){
+					var error = new Error( this.responseText );
+					console.error( error );
+
+					callback( error );
+
+				}else{
+					callback( null, [ ] );
+				}
+			}
+		},
+		function requestOverride( request ){
+			var bearer = [ "Bearer", accessToken ].join( " " )
+			request.setRequestHeader( "Authorization", bearer );
+
+			request.setRequestHeader( "Content-Length", DSIDParameter.length );
+
+			request.ontimeout = function onTimeout( ){
+				var error = new Error( "request timed out" );
+				console.error( error );
+
+				callback( error );
+			};
+
+			return DSIDParameter;
+		} );
 };
 
 Drop.prototype.getCollectionList = function getCollectionList( callback ){
 	var accessToken = this.accessToken;
 
 	boom( "POST", "https://api.dropbox.com/1/datastores/list_datastores",
-		function catcher( ){
-			if( this.status == 200 ){
-				console.debug( 200, this );
+		function catcher( error ){
+			if( error ){
+				callback( error );
+				return;
 			}
-			console.debug( this );
+
+			if( this.readyState == 4 ){
+				if( this.status == 200 ){
+					var result = JSON.parse( this.responseText );
+					callback( null, result.datastores );
+
+				}else if( this.status >= 400 ){
+					var error = new Error( this.responseText );
+					console.error( error );
+
+					callback( error );
+
+				}else{
+					callback( null, [ ] );
+				}
+			}
 		},
 		function requestOverride( request ){
 			var bearer = [ "Bearer", accessToken ].join( " " )
 			request.setRequestHeader( "Authorization", bearer );
+
+			request.ontimeout = function onTimeout( ){
+				var error = new Error( "request timed out" );
+				console.error( error );
+
+				callback( error );
+			};
 		} );
 };
 
 Drop.prototype.setPrimaryCollection = function setPrimaryCollection( collectionName ){
 	this.primaryCollection = collectionName;
 	Drop.primaryCollection = collectionName;
-}
+};
 
 Drop.prototype.get = function get( ){
 
